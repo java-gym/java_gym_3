35c5d3565dd803ef29e4856d80918c0e5692ef53
Mark <mverleg.noreply@gmail.com>
2019-09-30 19:57:43 +0200

    Add more tests about duplicates

--- src/test/java/noedit/SolutionTest.java
+++ src/test/java/noedit/SolutionTest.java
@@ -255,4 +255,121 @@ class SolutionTest {
         assertEquals((Integer) 2, solution.lookup(Data.of(192)).get());
         assertEquals((Integer) 2, solution.lookup(Data.of(191)).get());
     }
+
+    @Test
+    void testDuplicateDataConsecutive() {
+        var dataStream = DataStream.of(3, 1, 1, 2);
+        var registers = new Registers(1, 1, 1);
+        Solution solution = storeWithSingleLookup(registers, dataStream, Data.of(4), null);
+
+        assertEquals(Integer.valueOf(1), solution.lookup(Data.of(1)).get());
+        assertEquals(Integer.valueOf(0), solution.lookup(Data.of(2)).get());
+        assertEquals(Integer.valueOf(2), solution.lookup(Data.of(3)).get());
+    }
+
+    @Test
+    void testDuplicateDataSpread() {
+        var dataStream = DataStream.of(1, 3, 1, 2);
+        var registers = new Registers(1, 1, 1);
+        Solution solution = storeWithSingleLookup(registers, dataStream, Data.of(4), null);
+
+        assertEquals(Integer.valueOf(1), solution.lookup(Data.of(1)).get());
+        assertEquals(Integer.valueOf(0), solution.lookup(Data.of(2)).get());
+        assertEquals(Integer.valueOf(2), solution.lookup(Data.of(3)).get());
+    }
+
+    @Test
+    void testDuplicateDataMore() {
+        var dataStream = DataStream.of(1, 3, 4, 1, 1, 1, 2, 3);
+        var registers = new Registers(1, 1, 1);
+        Solution solution = storeWithSingleLookup(registers, dataStream, Data.of(5), null);
+
+        assertEquals(Integer.valueOf(2), solution.lookup(Data.of(1)).get());
+        assertEquals(Integer.valueOf(1), solution.lookup(Data.of(2)).get());
+        assertEquals(Integer.valueOf(0), solution.lookup(Data.of(3)).get());
+        assertEquals(Integer.valueOf(3), solution.lookup(Data.of(4)).get());
+    }
+
+    @Test
+    void testDuplicateLotsOfRandomItems() {
+        var dataStream = DataStream.of(80, 35, 77, 82, 14, 39, 66, 9, 79, 6, 74, 51, 92, 48, 42, 84, 68,
+                11, 5, 73, 81, 22, 75, 67, 32, 57, 78, 43, 10, 97, 83, 21, 38, 28, 23, 88, 90, 96, 49,
+                58, 50, 18, 59, 45, 37, 1, 1, 1, 1, 27, 15, 53, 46, 17, 56, 76, 30, 63, 33, 64, 31, 86,
+                95, 72, 64, 53, 38, 71, 58, 51, 84, 39, 91, 77, 85, 14, 66, 7, 11, 55, 10, 22, 92, 1,
+                76, 37, 27, 16, 2, 29, 88, 9, 89, 36, 45, 8, 46, 21, 62, 57, 47, 41, 17, 32, 73, 3, 52,
+                97, 61, 79, 44, 54, 63, 24, 26, 80, 30, 5, 34, 96, 31, 50, 67, 78, 18, 20, 93, 6, 28,
+                75, 69, 83, 19, 42, 59, 12, 60, 90, 68, 98, 74, 48, 43, 82, 100, 35, 56, 41, 52, 63,
+                31, 39, 27, 93, 2, 67, 14, 98, 73, 71, 65, 12, 17, 90, 86, 87, 61, 48, 26, 92, 85, 74,
+                5, 99, 81, 70, 75, 6, 35, 36, 22, 10, 64, 21, 80, 57, 44, 24, 72, 30, 66, 53, 84, 18,
+                79, 4, 47, 7, 96, 43, 49, 95, 55, 15, 45, 77, 37, 100, 19, 20, 97, 62, 58, 83, 59, 69,
+                32, 34, 94, 13, 88, 29, 33, 25, 46, 76, 8, 89, 65, 81, 49, 25, 4, 40, 36, 87, 60, 86,
+                85, 4, 29, 61, 94, 95, 44, 7, 89, 25, 8, 52, 70, 2, 47, 72, 91, 99, 93, 10, 35, 42, 43,
+                54, 94, 83, 31, 87, 51, 98, 92, 95, 23, 61, 48, 12, 89, 40, 86, 63, 41, 21, 82, 71, 13,
+                74, 16, 53, 15, 2, 79, 50, 58, 90, 8, 96, 3, 28, 84, 5, 69, 33, 62, 37, 18, 45, 68, 80,
+                17, 67, 32, 34, 38, 46, 77, 60, 22, 78, 29, 49, 70, 76, 47, 44, 30, 9, 55, 39, 56, 7,
+                64, 65, 85, 91, 24, 93, 11, 19, 88, 16, 20, 41, 55, 26, 54, 98, 24, 65);
+        var registers = new Registers(2, 5, 1, 1, 8, 11, 12, 9, 2, 8, 1);
+        Solution solution = storeWithSingleLookup(registers, dataStream, Data.of(0), null);
+
+        assertEquals(Integer.valueOf(10), solution.lookup(Data.of(13)).get());
+        assertEquals(Integer.valueOf(9), solution.lookup(Data.of(74)).get());
+        assertEquals(Integer.valueOf(9), solution.lookup(Data.of(53)).get());
+        assertEquals(Integer.valueOf(9), solution.lookup(Data.of(15)).get());
+        assertEquals(Integer.valueOf(9), solution.lookup(Data.of(2)).get());
+        assertEquals(Integer.valueOf(9), solution.lookup(Data.of(79)).get());
+        assertEquals(Integer.valueOf(9), solution.lookup(Data.of(50)).get());
+        assertEquals(Integer.valueOf(9), solution.lookup(Data.of(58)).get());
+        assertEquals(Integer.valueOf(9), solution.lookup(Data.of(90)).get());
+        assertEquals(Integer.valueOf(8), solution.lookup(Data.of(8)).get());
+        assertEquals(Integer.valueOf(8), solution.lookup(Data.of(96)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(3)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(28)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(84)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(5)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(69)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(33)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(62)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(37)).get());
+        assertEquals(Integer.valueOf(7), solution.lookup(Data.of(18)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(45)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(68)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(80)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(17)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(67)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(32)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(34)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(38)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(46)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(77)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(60)).get());
+        assertEquals(Integer.valueOf(6), solution.lookup(Data.of(22)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(78)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(29)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(49)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(70)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(76)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(47)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(44)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(30)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(9)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(39)).get());
+        assertEquals(Integer.valueOf(5), solution.lookup(Data.of(56)).get());
+        assertEquals(Integer.valueOf(4), solution.lookup(Data.of(7)).get());
+        assertEquals(Integer.valueOf(4), solution.lookup(Data.of(64)).get());
+        assertEquals(Integer.valueOf(4), solution.lookup(Data.of(85)).get());
+        assertEquals(Integer.valueOf(4), solution.lookup(Data.of(91)).get());
+        assertEquals(Integer.valueOf(4), solution.lookup(Data.of(93)).get());
+        assertEquals(Integer.valueOf(4), solution.lookup(Data.of(11)).get());
+        assertEquals(Integer.valueOf(4), solution.lookup(Data.of(19)).get());
+        assertEquals(Integer.valueOf(4), solution.lookup(Data.of(88)).get());
+        assertEquals(Integer.valueOf(3), solution.lookup(Data.of(16)).get());
+        assertEquals(Integer.valueOf(2), solution.lookup(Data.of(20)).get());
+        assertEquals(Integer.valueOf(1), solution.lookup(Data.of(41)).get());
+        assertEquals(Integer.valueOf(1), solution.lookup(Data.of(55)).get());
+        assertEquals(Integer.valueOf(1), solution.lookup(Data.of(26)).get());
+        assertEquals(Integer.valueOf(1), solution.lookup(Data.of(54)).get());
+        assertEquals(Integer.valueOf(1), solution.lookup(Data.of(98)).get());
+        assertEquals(Integer.valueOf(0), solution.lookup(Data.of(24)).get());
+        assertEquals(Integer.valueOf(0), solution.lookup(Data.of(65)).get());
+    }
 }
